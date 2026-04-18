8a5aa1185b4d49fc7c2fe3f4710761b663cff014
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-18 11:49:47 +0530

    add Student class with attributes, constructor, and methods for OOP demonstration

---
+++ A_OppDefinition/Student.java
@@ -0,0 +1,34 @@
+package JavaOopConcept.A_OppDefinition;
+
+public class Student {
+//    How to create an object in java
+
+//    1. Data members / Attributes
+    public int id;
+    public int age;
+    public String name;
+
+//    Constructor -> sets attribute value
+//    Default constructor -> default value => garbage / 0 / null
+    public Student() {
+        System.out.println("Default constructor is called");
+    }
+
+
+    //    2. Methods / Behavior
+    public void study(){
+        System.out.println("Student is studying...");
+    }
+
+    public void dance() {
+        System.out.println("Student is dancing...");
+    }
+
+    public void sleep() {
+        System.out.println("Student is sleeping...");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
