51217fcff3653bb06c846f466adb1f0cc349eb97
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-19 10:28:17 +0530

    add JavaAccessModifiers class to illustrate access modifiers in Java

---
+++ B_AccessModifiries/JavaAccessModifiers.java
@@ -0,0 +1,23 @@
+package JavaOopConcept.B_AccessModifiries;
+
+public class JavaAccessModifiers {
+    public static void main(String[] args) {
+        /*
+            Access Modifiers in Java – Complete Guide
+            Access Modifiers in Java control the visibility and accessibility of classes, methods, and variables in
+            different parts of a program. They help in data hiding, encapsulation, and security.
+
+            1. What are Access Modifiers?
+            Access Modifiers in Java are keywords that define the scope of a class, method, or variable.
+            They determine who can access them.
+
+            There are four types of access modifiers in Java:
+            private – Only within the same class.
+            default (no keyword) – Accessible within class and in the same package.
+            protected – Accessible within the same package, Class and subclass.
+            public – Accessible from anywhere in the program also outside the package.
+
+
+        */
+    }
+}
