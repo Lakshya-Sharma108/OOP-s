7c6afeb8dacdfbfba2f80dbea27d4b175782ad6e
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-20 13:09:08 +0530

    add Practice1 class with interview questions on compile time polymorphism

---
+++ F_Polymorphism/Practice1.java
@@ -0,0 +1,16 @@
+package JavaOopConcept.F_Polymorphism;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        /*
+                                    Interview Based Question's
+
+            Compile time Polymorphism :
+            1. What is Compile time Polymorphism ?
+            2. What is Method Overloading ?
+            3. What is Early Binding ?
+
+
+        */
+    }
+}
