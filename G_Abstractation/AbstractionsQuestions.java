900c9bdba6ff21095aa7960c7cc70c36994aa5bb
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-21 11:04:20 +0530

    add AbstractionsQuestions class with basic, code-based, and conceptual questions on abstraction in Java

---
+++ G_Abstractation/AbstractionsQuestions.java
@@ -0,0 +1,31 @@
+package JavaOopConcept.G_Abstractation;
+
+public class AbstractionsQuestions {
+    /*
+        ✅ Basic-Level Questions
+        What is abstraction in Java?
+        Why do we use abstraction?
+        What is the difference between abstract class and interface?
+        Can we create an object of an abstract class? Why or why not?
+        Can an abstract class have a constructor?
+        Can abstract classes have non-abstract methods?
+
+
+        ✅ Code-Based Questions
+        Write an abstract class Vehicle with an abstract method startEngine() and a non-abstract method fuelType(). Then create a class Car that extends Vehicle.
+        What will happen if a subclass does not implement all abstract methods of the abstract class?
+        Can we declare an abstract method as static or final? Why not?
+
+
+        ✅ Conceptual Comparison Questions
+        When would you use an abstract class over an interface?
+        Can we have both abstract and non-abstract methods in an abstract class? Give an example.
+        Difference between abstraction and encapsulation.
+
+
+        ✅ Tricky But Common
+        Can an interface have method implementation in Java? (Java 8 and above)
+        If a class has even one abstract method, what must we do with the class?
+        Can abstract classes implement interfaces?
+    */
+}
