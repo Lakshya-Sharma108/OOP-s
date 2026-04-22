fb15a923c9956530df7c1358e31867390ffbb082
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-22 19:41:33 +0530

    add ObjectMakingClass to demonstrate Employee creation using Builder pattern

---
+++ J_InnerClass/ImportantQuestions.java
@@ -0,0 +1,23 @@
+package JavaOopConcept.J_InnerClass;
+
+public class ImportantQuestions {
+    /*
+        TODO --> Common Interview Questions
+
+        Easy:
+        What is an inner class?
+        How many types of inner classes are there?
+        Can inner classes use private variables of outer class?
+
+        Medium:
+        Difference between static nested and non-static inner class?
+        Can local inner classes use local variables?
+        What does OuterClass.this mean?
+
+        Hard:
+        What is an anonymous class?
+        What is an effectively final variable?
+        Can inner classes have static methods?
+
+    */
+}
