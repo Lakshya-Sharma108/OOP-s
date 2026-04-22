d67a1102436fa7453fca73549eebea51a91ef070
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-22 19:36:21 +0530

    add Coupling class to illustrate the concepts of loose and tight coupling in Java

---
+++ H_Coupling/Coupling.java
@@ -0,0 +1,40 @@
+package JavaOopConcept.H_Coupling;
+
+public class Coupling {
+    public static void main(String[] args) {
+        /*
+            What is Coupling in Java (or any programming)?
+            Coupling means how much one class or module is dependent on another.
+            It’s about how tightly connected two parts of your code are.
+
+            Think of it like this (Real-life Example):
+            Imagine a TV remote.
+            If your remote works only with one specific brand of TV → they are tightly coupled.
+            But if your remote works with many TVs → they are loosely coupled.
+
+            Loose coupling means more flexibility, easy to maintain, and less chance of breaking things.
+
+
+
+            Types of Coupling :
+            1. Loose Coupling (Good Practice)
+               Classes are independent.
+               They interact through interfaces, abstract classes, or methods with general types.
+               Easy to change, test, and reuse.
+
+            2. Tight Coupling (Not Recommended)
+               One class directly depends on another specific class.
+               Hard to change, test, or reuse.
+
+
+            Why Loose Coupling is Better?
+            Easier to test
+            Easier to change or extend
+            Makes code more reusable and modular
+            Follows SOLID principles (like Dependency Inversion)
+
+
+
+        */
+    }
+}
